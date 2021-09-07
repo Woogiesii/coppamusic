2f09a3eae7ac4513a497a78c5ec3a2e624d3d9fa
Syifa Abyananta <70749185+Woogiesii@users.noreply.github.com>
2021-09-07 20:11:17 +0700

    Update invite.js

--- commands/Information/invite.js
+++ commands/Information/invite.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "invite",
     category: "Information",
     aliases: [ "addme" ],
-    description: "invite LavaMusic",
+    description: "invite CoppaMusic",
     args: false,
     usage: "",
     permission: [],
@@ -18,19 +18,15 @@ module.exports = {
     .setLabel("Invite")
     .setStyle("LINK")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
-			new MessageButton()
+	new MessageButton()
     .setLabel("GitHub")
     .setStyle("LINK")
-    .setURL("https://github.com/brblacky/lavamusic"),
-    new MessageButton()
-    .setLabel("Support")
-    .setStyle("LINK")
-    .setURL("https://discord.gg/gfcv94hDhv")
+    .setURL("https://github.com/brblacky/lavamusic")
 			);
 
           const mainPage = new MessageEmbed()
-            .setAuthor('LavaMusic', 'https://media.discordapp.net/attachments/845318824323448882/876690332333514752/1629089649835.png')
-            .setThumbnail('https://media.discordapp.net/attachments/845318824323448882/876690332333514752/1629089649835.png')
+            .setAuthor('CoppaMusic', 'https://media.discordapp.net/attachments/708683759599484988/868750391783862342/logo-coppa.png')
+            .setThumbnail('https://media.discordapp.net/attachments/708683759599484988/868750391783862342/logo-coppa.png')
              .setColor('#303236')
             .addField('invite lavamusic', `[Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot)`, true)
            message.channel.send({embeds: [mainPage], components: [row]})
