935e38e8a29c65d15ec087c13e3e20d057be914d
Syifa Abyananta <70749185+Woogiesii@users.noreply.github.com>
2021-09-07 19:54:10 +0700

    Add files via upload

---
+++ commands/Information/about.js
@@ -0,0 +1,36 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+
+module.exports = {
+    name: "about",
+    category: "Information",
+    aliases: [ "botinfo" ],
+    description: "See description about this project",
+    args: false,
+    usage: "",
+    permission: [],
+    owner: false,
+    execute: async (message, args, client, prefix) => {
+     
+    const row = new MessageActionRow()
+			.addComponents(
+        new MessageButton()
+    .setLabel("Invite")
+    .setStyle("LINK")
+    .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
+		new MessageButton()
+    .setLabel("GitHub")
+    .setStyle("LINK")
+    .setURL("https://github.com/woogiesii/coppamusic")
+			);
+
+      const mainPage = new MessageEmbed()
+            .setAuthor('CoppaMusic', 'https://media.discordapp.net/attachments/708683759599484988/868750391783862342/logo-coppa.png')
+            .setColor('#40B24A')
+            .addField('Creator', '[Woogiesii#1140](https://github.com/woogiesii)', true)
+            .addField('Repository', '[Here](https://github.com/woogiesii/coppamusic)', true)
+            .addField('\u200b',
+                `[CoppaMusic](https://github.com/woogiesii/coppamusic/) is [Blacky](https://github.com/brblacky)'s Was created by brblacky. He really wants to make his first open source project ever. Because he wants more for coding experience. In this project, he was challenged to make project with less bugs. Hope you enjoy using LavaMusic!`
+            )
+        return message.channel.send({embeds: [mainPage], components: [row]});
+    }
+}
